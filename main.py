ace5880fdb9d1b082122c0e93ab178f2140993f3
ahmadsky007 <s25960@pjwstk.edu.pl>
2024-03-21 19:58:36 +0100

    task 2 function for squares

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-squares = [x**2 for x in range(1, 11)]
-print(squares)
+def e_squares(begin, end):
+    squares = [x ** 2 for x in range(begin, end + 1)]
+    return squares
+
+
+if __name__ == "__main__":
+    first_num = int(input("Enter begin num1: "))
+    second_num = int(input("Enter ending num2: "))
+
+    output = e_squares(first_num, second_num)
+
+    print(output)
