3e0431f5fc4e44fc4b509e49aeb78b76d720d235
ahmadsky007 <s25960@pjwstk.edu.pl>
2024-03-21 20:03:38 +0100

    task 3 create square generator class

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-def e_squares(begin, end):
-    squares = [x ** 2 for x in range(begin, end + 1)]
-    return squares
+class SquareGenerator:
+    @staticmethod
+    def generate_squares(start, end):
+        return [x ** 2 for x in range(start, end + 1)]
 
 
 if __name__ == "__main__":
+    square_generator = SquareGenerator()
     first_num = int(input("Enter begin num1: "))
     second_num = int(input("Enter ending num2: "))
 
-    output = e_squares(first_num, second_num)
+    output = square_generator.generate_squares(first_num, second_num)
 
     print(output)
